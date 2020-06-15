97a6a5b48c381f3a08e9cd5d44fa47f9962779c4
Evan Sheffield <evan.c.sheffield@gmail.com>
2020-06-15 13:49:11 -0400

    Updates

---
+++ precache-manifest.3fea1a3c1f297ea4b0e2a75c208340f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd66960cd8f395d17c8277a05736d7e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f1ea49bdb4a996c3ce10",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "bc0facd4635f4099a29e",
+    "url": "./static/js/2.30317e51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.30317e51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ea49bdb4a996c3ce10",
+    "url": "./static/js/main.fcee9372.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.fcee9372.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3161df1395578aeaa966",
+    "url": "./static/js/runtime-main.b2b60ed6.js"
+  }
+]);

f965ebb168473ab00b0087c8e89f79c3187ece95
Evan Sheffield <evan.c.sheffield@gmail.com>
2020-06-15 16:15:32 -0400

    fix: Use HTTPS for example dashboard

--- example/src/App.js
+++ example/src/App.js
@@ -6,7 +6,7 @@ import 'ivh-react-tableau/dist/index.css'
 const App = () => {
   return (
     <IvhTableauDashboard
-      url='http://public.tableau.com/views/RegionalSampleWorkbook/College'
+      url='https://public.tableau.com/views/RegionalSampleWorkbook/College'
       filters={{
         'Academic Year': '2013'
       }}
